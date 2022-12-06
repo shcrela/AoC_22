dbd9f9f53b1fd9e43e81b4ea86cdee534dd9f56c
shcrela <shcrela@gmail.com>
2022-12-06 14:55:57 +0100

    added docstring for get_data func

--- helper.py
+++ helper.py
@@ -4,6 +4,12 @@ import requests
 import numpy as np
 
 def get_data(day):
+    """Once you're logged in to AoC website,
+    you can find your session ID in the cookie
+    (In the browser, Press F12 -> Application).
+    I then stored mine in environment variable
+    called `AOC22_SESSIONID`."""
+    
     SESSIONID = os.environ.get("AOC22_SESSIONID")
     uri = f'http://adventofcode.com/2022/day/{day}/input'
     response = requests.get(uri, cookies={'session': SESSIONID})
